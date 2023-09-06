fb33913f366248d883e28589a583d349717d2541
Michael Haeuslmann <michael.haeuslmann@gmail.com>
2023-09-06 11:01:27 +0200

    Implement test for addExhaustionCardsToOtherPlayersOption

---
+++ tests/options/addExhaustionCardsToOtherPlayersOption.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from "node:test"
+import { deepEqual } from "node:assert"
+import RandomPlayer from "../../src/randomPlayer.js"
+import GameBoard from "../../src/gameBoard.js"
+import { AddExhaustionCardsToOtherPlayersOption } from "../../src/options/addExhaustionCardsToOtherPlayersOption.js"
+import { ExhaustionCard } from "../../src/cards.js"
+
+describe("Add Exhaustion Cards to Other Players Option", () => {
+	it("should add exhaustion cards to discard piles of all other players", () => {
+		const one = new RandomPlayer("One")
+		const two = new RandomPlayer("Two")
+		const three = new RandomPlayer("Three")
+		const four = new RandomPlayer("Fours")
+		const gameBoard = new GameBoard([one, two, three, four])
+		new AddExhaustionCardsToOtherPlayersOption().resolve(gameBoard, one)
+		deepEqual(one.discardedCards, [])
+		deepEqual(two.discardedCards, [new ExhaustionCard()])
+		deepEqual(three.discardedCards, [new ExhaustionCard()])
+		deepEqual(four.discardedCards, [new ExhaustionCard()])
+	})
+})
