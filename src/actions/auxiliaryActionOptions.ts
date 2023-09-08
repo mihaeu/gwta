a50a54097fec0150c7d55f954780c2cce229ea86
Michael Haeuslmann <michael.haeuslmann@gmail.com>
2023-09-08 15:19:04 +0200

    Add missing single auxiliary action

--- src/actions/auxiliaryActionOptions.ts
+++ src/actions/auxiliaryActionOptions.ts
@@ -6,6 +6,12 @@ import { DrawCardOption } from "../options/drawCardOption.js"
 import { FirstThanSecondsOption } from "../options/firstThanSecondOption.js"
 import { DiscardCardOptions } from "./discardCardOptions.js"
 import { Building } from "../buildings/building.js"
+import { GainGrainOption } from "../options/gainGrainOption.js"
+import { CompoundOption } from "../options/compoundOption.js"
+import { CertificateOption } from "../options/certificateOption.js"
+import { MoveTrainOptions } from "./moveTrainOptions.js"
+import { RemoveCardOption } from "../options/removeCardOption.js"
+import { AnyCard } from "../cards.js"
 
 export class AuxiliaryActionOptions extends Option {
 	constructor(building?: Building) {
@@ -13,10 +19,19 @@ export class AuxiliaryActionOptions extends Option {
 		this._building = building
 	}
 
-	/**
-	 * TODO this is missing the check for available actions
-	 */
 	resolve(gameBoard: GameBoard, currentPlayer: Player): Option[] {
-		return [new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions()), new GainCoinOption(1)]
+		const options: Option[] = [new GainCoinOption(1), new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions())]
+
+		if (currentPlayer.coins > 0) {
+			options.push(new FirstThanSecondsOption(new GainCoinOption(-1), new GainGrainOption(1)))
+			options.push(new FirstThanSecondsOption(new GainCoinOption(-1), new MoveTrainOptions(1)))
+			options.push(new FirstThanSecondsOption(new GainCoinOption(-1), new RemoveCardOption(new AnyCard())))
+		}
+
+		if (currentPlayer.grain > 0) {
+			options.push(new CompoundOption(new GainGrainOption(-1), new CertificateOption(1), new GainCoinOption(1)))
+		}
+
+		return options
 	}
 }
--- tests/actions/auxiliaryActionOptions.test.ts
+++ tests/actions/auxiliaryActionOptions.test.ts
@@ -2,11 +2,16 @@ import { describe, it } from "node:test"
 import { deepEqual } from "node:assert"
 import { gameBoardWithTwoPlayers } from "../testUtils.js"
 import { AuxiliaryActionOptions } from "../../src/actions/auxiliaryActionOptions.js"
-import { HolandoArgentino, Patagonico } from "../../src/cards.js"
+import { AnyCard, HolandoArgentino, Patagonico } from "../../src/cards.js"
 import { DrawCardOption } from "../../src/options/drawCardOption.js"
 import { GainCoinOption } from "../../src/options/gainCoinOption.js"
 import { FirstThanSecondsOption } from "../../src/options/firstThanSecondOption.js"
 import { DiscardCardOptions } from "../../src/actions/discardCardOptions.js"
+import { GainGrainOption } from "../../src/options/gainGrainOption.js"
+import { MoveTrainOptions } from "../../src/actions/moveTrainOptions.js"
+import { RemoveCardOption } from "../../src/options/removeCardOption.js"
+import { CompoundOption } from "../../src/options/compoundOption.js"
+import { CertificateOption } from "../../src/options/certificateOption.js"
 
 describe("Auxiliary Action Options", () => {
 	it("should list gain coins and draw card discard card action", () => {
@@ -14,8 +19,34 @@ describe("Auxiliary Action Options", () => {
 		one.handCards.push(new Patagonico())
 		one.handCards.push(new HolandoArgentino())
 		deepEqual(new AuxiliaryActionOptions().resolve(gameBoard, one), [
-			new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions()),
 			new GainCoinOption(1),
+			new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions()),
+		])
+	})
+
+	it("should contain options that require coins if player has at least one coin", () => {
+		const { gameBoard, one } = gameBoardWithTwoPlayers()
+		one.handCards.push(new Patagonico())
+		one.handCards.push(new HolandoArgentino())
+		one.gainCoins(1)
+		deepEqual(new AuxiliaryActionOptions().resolve(gameBoard, one), [
+			new GainCoinOption(1),
+			new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions()),
+			new FirstThanSecondsOption(new GainCoinOption(-1), new GainGrainOption(1)),
+			new FirstThanSecondsOption(new GainCoinOption(-1), new MoveTrainOptions(1)),
+			new FirstThanSecondsOption(new GainCoinOption(-1), new RemoveCardOption(new AnyCard())),
+		])
+	})
+
+	it("should contain options that require grain if player has at least one grain", () => {
+		const { gameBoard, one } = gameBoardWithTwoPlayers()
+		one.handCards.push(new Patagonico())
+		one.handCards.push(new HolandoArgentino())
+		one.gainGrain(1)
+		deepEqual(new AuxiliaryActionOptions().resolve(gameBoard, one), [
+			new GainCoinOption(1),
+			new FirstThanSecondsOption(new DrawCardOption(), new DiscardCardOptions()),
+			new CompoundOption(new GainGrainOption(-1), new CertificateOption(1), new GainCoinOption(1)),
 		])
 	})
 })
